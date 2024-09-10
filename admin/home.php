a0887802025e7d6e8bb0ff02c7e994dceb974ca3
Ted-Chachs <j12-1507-2018@student.mksu.ac.ke>
2024-09-10 14:19:34 +0700

    First Upload of SMS

--- admin/home.php
+++ admin/home.php
@@ -15,7 +15,6 @@
             </div>
             <!-- /.info-box-content -->
         </div>
-        <!-- /.info-box -->
     </div>
     <div class="col-12 col-sm-6 col-md-3">
         <div class="info-box bg-light shadow">
